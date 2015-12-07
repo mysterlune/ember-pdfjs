34d72b9877bf9a2eb86d719f0c7cef23640f9e4e
River Lune <mysterlune@gmail.com>
2015-12-07 09:39:01 -0800

    Adding improvements to the import, tree merge, and component availablitiy.

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-/* jshint node: true */
 'use strict';
 
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-pdfjs'
-};
+  name: 'ember-pdfjs',
+
+  treeFor: function() {
+
+    var trees = [];
+
+    trees.push(this._super.treeFor.apply(this, arguments));
+
+    var PDFJS = pickFiles('bower_components/pdfjs-dist/build',{
+        srcDir: '/',
+        files: ['pdf.js','pdf.worker.js'],
+        destDir: '/assets'
+    });
+
+    trees.push(PDFJS);
+
+    var PDFJSExtras = pickFiles('bower_components/pdfjs-dist/web',{
+        srcDir: '/',
+        files: ['compatibility.js'],
+        destDir: '/assets'
+    });
+
+    trees.push(PDFJSExtras)
+
+    var PDFJSCmaps = pickFiles('bower_components/pdfjs-dist/cmaps',{
+        srcDir: '/',
+        files: ['**/*.bcmap'],
+        destDir: '/assets/web/cmaps'
+    });
+
+    trees.push(PDFJSCmaps);
+
+    return mergeTrees(trees);
+  },
+
+  included: function(app, parentAddon) {
+
+    this._super.included.apply(this, arguments);
+
+    var target = (parentAddon || app);
+
+    target.import(target.bowerDirectory + '/pdfjs-dist/web/pdf_viewer.js');
+    target.import(target.bowerDirectory + '/pdfjs-dist/build/pdf.js');
+    target.import(target.bowerDirectory + '/pdfjs-dist/build/pdf.worker.js');
+
+  }
+}
